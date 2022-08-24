6d7b765c927054915acfb7283f453fcdb52fe5a3
tushar1527 <tushar.savaliya@centillionsofttech.com>
2022-08-24 11:49:43 +0530

    update

--- src/pages/chatScreen.js
+++ src/pages/chatScreen.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import socketClient from "socket.io-client";
-const SERVER = "http://54.163.188.182:8000";
+const SERVER = "http://localhost:8000";
 
 export const ChatScreen = () => {
   let socket = socketClient(SERVER);
 
   const [message, setMessage] = useState();
+  const [data, setData] = useState([]);
   let roomId = localStorage.getItem("roomId");
   socket.on("connection", () => {
     console.log(`I'm connected with the back-end`);
@@ -13,17 +14,28 @@ export const ChatScreen = () => {
   socket.emit("join", roomId, (ack) => {
     console.log(ack);
   });
-  socket.on(roomId, (data) => {
-    console.log("daya", data);
+  socket.on(roomId, (data1) => {
+    let array = [data];
+    let userId = localStorage.getItem("userID");
+    // array = data;
+    let record = (
+      <>
+        <p className={userId === data1.senderId ? "left" : "right"}>
+          {data1.content}
+        </p>
+      </>
+    );
+    array.push(record);
+    setData(array);
   });
 
   const sendMessage = async () => {
-    let data = {
+    let data2 = {
       roomId,
       content: message,
       senderId: localStorage.getItem("userId"),
     };
-    socket.emit("sendMessage", data);
+    socket.emit("sendMessage", data2);
     // let data = {
     //   senderId: localStorage.getItem("userId"),
     //   content: message,
@@ -36,7 +48,7 @@ export const ChatScreen = () => {
         <div className="header">
           <div className="name">{localStorage.getItem("username")}</div>
         </div>
-        <div className="chatScreen"></div>
+        <div className="chatScreen">{data}</div>
         <div className="input">
           <input
             type="text"
